92de29581d7571175f17a70f11ea34882ebd3b54
Flavio <flavio.fontes.ext@dasa.com.br>
2023-05-06 00:32:13 -0300

    Mudança no nome da variável

--- Escolha_time.py
+++ Escolha_time.py
@@ -3,9 +3,9 @@ import random
 nomes = ["nome1","nome2","nome3","nome4","nome5","nome6","nome7","nome8","nome9","nome10","nome11","nome12","nome13","nome14","nome15"]
 qtd_times = 3
 random.shuffle(nomes)
-sorteio = [nomes[i::qtd_times] for i in range(qtd_times)]
+separar_times = [nomes[i::qtd_times] for i in range(qtd_times)]
 
-times = list(sorteio)
+times = list(separar_times)
 indice = 1
 for time in times:
     print(f" Time {indice}: {time}")
