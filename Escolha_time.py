98bd2c681ff071fd659f8805c76e334b4784ea6f
Flavio <flavio.fontes.ext@dasa.com.br>
2023-05-06 00:20:04 -0300

    Update no algoritmo

--- Escolha_time.py
+++ Escolha_time.py
@@ -2,10 +2,11 @@ import random
 
 nomes = ["nome1","nome2","nome3","nome4","nome5","nome6","nome7","nome8","nome9","nome10","nome11","nome12","nome13","nome14","nome15"]
 qtd_times = 3
+random.shuffle(nomes)
 sorteio = [nomes[i::qtd_times] for i in range(qtd_times)]
-random.shuffle(sorteio)
-times = list(enumerate(sorteio))
+
+times = list(sorteio)
 indice = 1
 for time in times:
-    print(f" Time {indice}: {time[1]}")
+    print(f" Time {indice}: {time}")
     indice +=1
